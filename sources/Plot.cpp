0f5bfc0acda1d840f47be1b10a95a6c72e815da0
Artem Karpov <artem.ka1001pm@gmail.com>
2021-10-06 21:11:00 +0300

    Refactoring

--- sources/Plot.cpp
+++ sources/Plot.cpp
@@ -94,9 +94,9 @@ const char *FunctionManager::FunctionsManagerException::what() const
     return _message.c_str();
 }
 
-PlotManager::PlotManager(std::string windowTitle, sf::Vector2u windowSize, std::string fontPath) : _windowTitle(std::move(windowTitle)), _windowSize(windowSize), _fontPath(std::move(fontPath))
+PlotManager::PlotManager(std::string windowTitle, sf::Vector2u windowSize, const std::string& fontPath) : _windowTitle(std::move(windowTitle)), _windowSize(windowSize)
 {
-
+    _font.loadFromFile(fontPath);
 }
 
 void PlotManager::setPoints(std::vector<Point2f> points) noexcept
@@ -111,126 +111,42 @@ const std::vector<Point2f> &PlotManager::getPoints() const noexcept
 
 void PlotManager::display(float left, float right, float bottom, float top)
 {
+    PlotInformation plotInformation;
+    
     sf::RenderWindow window(sf::VideoMode(_windowSize.x, _windowSize.y), _windowTitle.c_str(), sf::Style::Close | sf::Style::Titlebar);
     sf::Clock frameTime;
-    sf::Event event;
     
-    sf::Font font;
-    sf::Text mousePositionText, plotInformationText;
+    sf::Vector2i windowMousePosition;
+    sf::Vector2i previousWindowMousePosition = sf::Mouse::getPosition(window);
     
-    sf::Vector2i mousePosition;
-    sf::Vector2i previousMousePosition = sf::Mouse::getPosition(window);
-    sf::Vector2f plotMousePosition;
-
-    window.setFramerateLimit(FRAME_RATE);
     glViewport(0, 0, (int)_windowSize.x, (int)_windowSize.y);
     
-    font.loadFromFile(_fontPath);
-    mousePositionText.setFont(font);
-    mousePositionText.setFillColor(sf::Color::Red);
-    mousePositionText.setCharacterSize(TEXT_SIZE);
-    plotInformationText.setFont(font);
-    plotInformationText.setFillColor(sf::Color::Red);
-    plotInformationText.setCharacterSize(TEXT_SIZE);
+    plotInformation.left = left;
+    plotInformation.right = right;
+    plotInformation.bottom = bottom;
+    plotInformation.top = top;
     
     while(window.isOpen())
     {
-        while(window.pollEvent(event))
-        {
-            switch(event.type)
-            {
-                case sf::Event::Closed:
-                {
-                    window.close();
-                    
-                    break;
-                }
-                
-                case sf::Event::MouseWheelScrolled:
-                {
-                    if(event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
-                    {
-                        float delta = event.mouseWheelScroll.delta;
-                        
-                        if(right - left > -2.0 * delta && top - bottom > -2.0 * delta)
-                        {
-                            left -= delta;
-                            right += delta;
-                            bottom -= delta;
-                            top += delta;
-                        }
-                    }
-                    
-                    break;
-                }
-            }
-        }
+        _handleWindowEvents(plotInformation, window);
         
-        mousePosition = sf::Mouse::getPosition(window);
+        windowMousePosition = sf::Mouse::getPosition(window);
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
         {
-            sf::Vector2f positionDifferent;
-            
-            positionDifferent.x = (float)(mousePosition.x - previousMousePosition.x) * ((right - left) / (float)_windowSize.x);
-            positionDifferent.y = (float)(mousePosition.y - previousMousePosition.y) * ((top - bottom) / (float)_windowSize.y);
-            
-            left   -= positionDifferent.x;
-            right  -= positionDifferent.x;
-            bottom += positionDifferent.y;
-            top    += positionDifferent.y;
+            _move(plotInformation, windowMousePosition, previousWindowMousePosition);
         }
-        previousMousePosition = mousePosition;
+        previousWindowMousePosition = windowMousePosition;
         
         if(frameTime.getElapsedTime() > sf::milliseconds((signed int)(1000.0 / FRAME_RATE)))
         {
             frameTime.restart();
     
-            glLoadIdentity();
-            glOrtho(left, right, bottom, top, 1, -1);
-    
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            plotInformation.mousePosition = _plotMousePosition(plotInformation, windowMousePosition);
             
-            glBegin(GL_LINES);
-            {
-                glColor3f(1, 0, 0);
-                glVertex2f(left, 0);
-                glVertex2f(right, 0);
-        
-                glColor3f(0, 1, 0);
-                glVertex2f(0, bottom);
-                glVertex2f(0, top);
-            }
-            glEnd();
+            window.clear();
     
-            glBegin(GL_LINE_STRIP);
-            {
-                glColor3f(1, 1, 1);
-                for(const auto &point: _points)
-                {
-                    glVertex2f(point.x, point.y);
-                }
-            }
-            glEnd();
-    
-            plotInformationText.setString("left: " + std::to_string(left) + " right: " + std::to_string(right) + '\n' + "bottom: " + std::to_string(bottom) + " top: " + std::to_string(top));
-            plotInformationText.setPosition({0, 0});
-            
-            plotMousePosition.y = (float)_windowSize.y - (float)mousePosition.y;
-            plotMousePosition.x = (float)mousePosition.x;
-            plotMousePosition.x /= (float)_windowSize.x / (right - left);
-            plotMousePosition.y /= (float)_windowSize.y / (top - bottom);
-            plotMousePosition.x += left;
-            plotMousePosition.y += bottom;
-    
-            mousePositionText.setString("x: " + std::to_string(plotMousePosition.x) + " y: " + std::to_string(plotMousePosition.y));
-            mousePositionText.setPosition({0, 40});
-    
-            window.pushGLStates();
-            
-            window.draw(plotInformationText);
-            window.draw(mousePositionText);
-            
-            window.popGLStates();
+            _glDraw(plotInformation);
+            _sfmlDraw(plotInformation, window);
             
             window.display();
         }
@@ -255,6 +171,127 @@ const std::string &PlotManager::getWindowTitle() const noexcept
 sf::Vector2u PlotManager::getWindowSize() const noexcept
 {
     return _windowSize;
+}
+
+void PlotManager::_move(PlotInformation &plotInformation, sf::Vector2i windowMousePosition, sf::Vector2i previousWindowMousePosition) const
+{
+    static sf::Vector2f positionDifferent;
+    
+    positionDifferent.x = (float)(windowMousePosition.x - previousWindowMousePosition.x) * ((plotInformation.right - plotInformation.left) / (float)_windowSize.x);
+    positionDifferent.y = (float)(windowMousePosition.y - previousWindowMousePosition.y) * ((plotInformation.top - plotInformation.bottom) / (float)_windowSize.y);
+    
+    plotInformation.left   -= positionDifferent.x;
+    plotInformation.right  -= positionDifferent.x;
+    plotInformation.bottom += positionDifferent.y;
+    plotInformation.top    += positionDifferent.y;
+}
+
+void PlotManager::_handleWindowEvents(PlotManager::PlotInformation &plotInformation, sf::RenderWindow &window) const
+{
+    static sf::Event event;
+    
+    while(window.pollEvent(event))
+    {
+        switch(event.type)
+        {
+            case sf::Event::Closed:
+            {
+                window.close();
+                
+                break;
+            }
+            
+            case sf::Event::MouseWheelScrolled:
+            {
+                if(event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
+                {
+                    float delta = event.mouseWheelScroll.delta;
+                    
+                    if(plotInformation.right - plotInformation.left > 2.0 * delta && plotInformation.top - plotInformation.bottom > 2.0 * delta)
+                    {
+                        plotInformation.left   += delta;
+                        plotInformation.right  -= delta;
+                        plotInformation.bottom += delta;
+                        plotInformation.top    -= delta;
+                    }
+                }
+                
+                break;
+            }
+        }
+    }
+}
+
+sf::Vector2f PlotManager::_plotMousePosition(const PlotInformation &plotInformation, sf::Vector2i windowMousePosition) const
+{
+    sf::Vector2f plotMousePosition;
+    
+    plotMousePosition.x = (float)windowMousePosition.x;
+    plotMousePosition.y = (float)_windowSize.y - (float)windowMousePosition.y;
+    plotMousePosition.x /= (float)_windowSize.x / (plotInformation.right - plotInformation.left);
+    plotMousePosition.y /= (float)_windowSize.y / (plotInformation.top - plotInformation.bottom);
+    plotMousePosition.x += plotInformation.left;
+    plotMousePosition.y += plotInformation.bottom;
+    
+    return plotMousePosition;
+}
+
+void PlotManager::_glDraw(const PlotInformation &plotInformation) noexcept
+{
+    glLoadIdentity();
+    glOrtho(plotInformation.left, plotInformation.right, plotInformation.bottom, plotInformation.top, 1, -1);
+    
+    glBegin(GL_LINES);
+    {
+        glColor3f(1, 0, 0);
+        glVertex2f(plotInformation.left, 0);
+        glVertex2f(plotInformation.right, 0);
+        
+        glColor3f(0, 1, 0);
+        glVertex2f(0, plotInformation.bottom);
+        glVertex2f(0, plotInformation.top);
+    }
+    glEnd();
+    
+    glBegin(GL_LINE_STRIP);
+    {
+        glColor3f(1, 1, 1);
+        for(const auto &point: _points)
+        {
+            glVertex2f(point.x, point.y);
+        }
+    }
+    glEnd();
+}
+
+sf::Text PlotManager::_textInstance()
+{
+    sf::Text text;
+    
+    text.setFont(_font);
+    text.setFillColor(sf::Color::Red);
+    text.setCharacterSize(TEXT_SIZE);
+    
+    return text;
+}
+
+void PlotManager::_sfmlDraw(const PlotManager::PlotInformation &plotInformation, sf::RenderWindow &window) noexcept
+{
+    static auto mousePositionText = _textInstance();
+    static auto plotInformationText = _textInstance();
+    
+    plotInformationText.setString("left: " + std::to_string(plotInformation.left) + " right: " + std::to_string(plotInformation.right) + '\n' + "bottom: " + std::to_string(plotInformation.bottom) + " top: " + std::to_string(plotInformation.top));
+    plotInformationText.setPosition({0, 0});
+    
+    mousePositionText.setString("x: " + std::to_string(plotInformation.mousePosition.x) + " y: " + std::to_string(plotInformation.mousePosition.y));
+    mousePositionText.setPosition({0, 40});
+    
+    window.pushGLStates();
+    
+    window.draw(plotInformationText);
+    window.draw(mousePositionText);
+    
+    window.popGLStates();
 }
 
 PlotManager::PlotManagerException::PlotManagerException(std::string message) : _message(std::move(message))
--- sources/Plot.hpp
+++ sources/Plot.hpp
@@ -58,17 +58,15 @@ private:
 class PlotManager
 {
 public:
-    PlotManager(std::string windowTitle, sf::Vector2u windowSize, std::string fontPath);
+    PlotManager(std::string windowTitle, sf::Vector2u windowSize, const std::string& fontPath);
     
     void setPoints(std::vector<Point2f> points) noexcept;
     void setWindowTitle(std::string windowTitle) noexcept;
     void setWindowSize(sf::Vector2u windowSize) noexcept;
-    void setFontPath(std::string fontPath) noexcept;
     
     const std::vector<Point2f> &getPoints() const noexcept;
     const std::string &getWindowTitle() const noexcept;
     sf::Vector2u getWindowSize() const noexcept;
-    std::string getFontPath() const noexcept;
     
     void display(float left, float right, float bottom, float top);
     
@@ -85,15 +83,33 @@ public:
     };
     
 private:
+    struct PlotInformation
+    {
+        float left, right, bottom, top;
+        sf::Vector2f mousePosition;
+    };
+    
     std::vector<Point2f> _points;
     
     std::string _windowTitle;
     sf::Vector2u _windowSize;
     
-    std::string _fontPath;
+    sf::Font _font;
     
     static const unsigned FRAME_RATE = 60;
     static const unsigned TEXT_SIZE = 12;
+    
+    void _move(PlotInformation &plotInformation, sf::Vector2i windowMousePosition, sf::Vector2i previousWindowMousePosition) const;
+    
+    void _handleWindowEvents(PlotInformation &plotInformation, sf::RenderWindow &window) const;
+    
+    sf::Vector2f _plotMousePosition(const PlotInformation &plotInformation, sf::Vector2i windowMousePosition) const;
+    
+    void _glDraw(const PlotInformation &plotInformation) noexcept;
+    void _sfmlDraw(const PlotInformation &plotInformation, sf::RenderWindow &window) noexcept;
+    
+    sf::Text _textInstance();
+    
 };
 
 
